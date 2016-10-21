507269a577cda26897026861d1831d8d50540807
johanahlqvist <johan_ahlkvist@hotmail.com>
2016-10-21 19:16:57 +0200

    feat: added check if market is live

--- sources/yahoo.py
+++ sources/yahoo.py
@@ -31,9 +31,13 @@ class YahooRealTime(Source):
 
     def __init__(self, mongo):
         super().__init__('YahooRealTime', mongo)
+        self.symbol_market = {}
 
     def _download_data(self, symbols, params):
-        symbols = [s for s in symbols if self._is_trading(s)]
+        unknown_symbols = [s for s in symbols if s not in self.symbol_market]
+        symbols = [s for s in symbols if s not in self.symbol_market or
+                   self._is_trading(self.symbol_market.get(s))]
+        symbols.extend(unknown_symbols)
         if len(symbols) == 0:
             return []
 
@@ -71,12 +75,23 @@ class YahooRealTime(Source):
                 'data': {key: r[key] for key in DATA_KEYS if key in r},
                 'ticker': r['symbol']
             }
-            data.append(d)
+
+            # add missing symbol market data
+            ticker = d.get('ticker')
+            ticker_market = d.get('data').get('StockExchange')
+            if ticker_market is not None and ticker in unknown_symbols:
+
+                self.symbol_market[ticker] = ticker_market
+                if self._is_trading(self.symbol_market.get(ticker)):
+                    data.append(d)
+            else:
+                data.append(d)
         return data
 
-    def _is_trading(self, symbol):
-        # Currencies and commodities
-        if '=' in symbol:
+    def _is_trading(self, market):
+        # Currencies and commodities are always trading
+        always_open = ['CCY', 'CMX', 'NYM', 'CBT']
+        if any(market in m for m in always_open):
             return True
 
         d = datetime.datetime.now()
@@ -84,19 +99,20 @@ class YahooRealTime(Source):
         utc_time = tz.normalize(tz.localize(d)).astimezone(pytz.utc)
 
         # Stocks
+        # StockExchange callsign : [market timezone, market open, market close]
+        markets = {'STO': [pytz.timezone('Europe/Stockholm'),
+                           datetime.time(9, 00), datetime.time(17, 30)],
+                   'SNP': [pytz.timezone('US/Eastern'),
+                           datetime.time(9, 30), datetime.time(16, 00)],
+                   'NMS': [pytz.timezone('US/Eastern'),
+                           datetime.time(9, 30), datetime.time(16, 00)]}
 
-        # United States per default
-        symbol_timezone = pytz.timezone('US/Eastern')
-        symbol_open = datetime.time(9, 30)
-        symbol_close = datetime.time(16, 00)
+        if market not in markets:
+            print('No time data for market \'%s\', skipping.' % market)
+            return False
 
-        # Swedish
-        if '.ST' in symbol or '^OMX' in symbol:
-            symbol_timezone = pytz.timezone('Europe/Stockholm')
-            symbol_open = datetime.time(9, 00)
-            symbol_close = datetime.time(17, 30)
-
-        symbol_time = utc_time.astimezone(symbol_timezone)
-        return (symbol_time.time() > symbol_open and
-                symbol_time.time() < symbol_close and
-                symbol_time.isoweekday() in range(1, 5))
+        market_info = markets.get(market)
+        market_time = utc_time.astimezone(market_info[0])
+        return (market_time.time() > market_info[1] and
+                market_time.time() < market_info[2] and
+                market_time.isoweekday() in range(1, 6))
